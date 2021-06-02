9f04f5f00765db0b8d8e1dbd5454bf586acab058
Skoczylas_Justyna <justynaklys95@gmail.com>
2021-06-02 19:27:57 +0200

    Prosta funkcja szyfrująca tekst

--- GitHub.py
+++ GitHub.py
@@ -1,2 +1,17 @@
 #Zrobimy program do szyfrowania
 #Funkcja szyfrowanie, deszyfrowanie i jakieś menu
+
+
+# funkcja szyfrująca tekst
+def szyfrowanie(tekst):
+    for i in range(len(tekst)):
+        if (tekst[i] == "a"):
+            tekst[i] = "y"
+        elif (tekst[i] == "e"):
+            tekst[i] = "i"
+        elif (tekst[i] == "i"):
+            tekst[i] = "o"
+        elif (tekst[i] == "o"):
+            tekst[i] = "a"
+        elif (tekst[i] == "y"):
+            tekst[i] = "e"
